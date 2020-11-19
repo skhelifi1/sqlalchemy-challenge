eee9c881735030776c00f7c3c1c4ca3a3e7b1d95
Sirine <sirine.khelifi1@outlook.com>
2020-11-18 23:05:18 -0500

    flask in the works

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ def Home page():
         f"/api/v1.0/precipitation>"
         f"/api/v1.0/stations"
         f"/api/v1.0/tobs"
-        f"/api/v1.0/stations"
-    )
+        )
 
 
 @app.route("/api/v1.0/precipitation>")
@@ -31,39 +30,40 @@ def precipitation():
     session = Session(engine)
 
     """Return a list of dates and its corresponding precipitation"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-     = list(np.ravel(results))
-
-    return jsonify(all_names)
-
-
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    
+    precip_list= session.query(Measurement.date, Measurement.prcp).filter(Measurement.date>=one_year_ago)
 
     session.close()
 
     # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+   date_precip= []
+    for date, prcp in precip_list:
+        date_precip_dict = {}
+        date_dict["date"] = date
+        precip_dict["prcp"] = prcp
+        date_precip.append(date_precip_dict)
 
-    return jsonify(all_passengers)
+    return jsonify(date_precip)
+    
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
+    """Return a list of stations from the dataset"""
+    stations_list = session.query(Measurement.station, func.count(Measurement.station)).\
+group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+    session.close()
+    # convert a tuple list into a normal list
+    stations_lst= list(np.ravel(stations_list))
+    return jsonify(stations_lst)
 
+@app.route("/api/v1.0/tobs")
+def tobs(:
+    session =Session(engine)
+    """Return the dates and temperature observations of the most active station for the last year of data"""
+
+    temp_list=session.query(Measurement.tobs).filter(Measurement.station=='USC00519281').filter(Measurement.date>=one_year_ago).all()
+    session.close()
 if __name__ == '__main__':
     app.run(debug=True)
